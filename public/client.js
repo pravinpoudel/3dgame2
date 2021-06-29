8c4f0d7704176f04b71b59a595ec71b2ce6e53a2
pravinpoudel <prvnpoudel4@gmail.com>
2021-06-29 22:23:11 +0545

    model added

--- public/client.js
+++ public/client.js
@@ -2,58 +2,119 @@
 // import { OrbitControls } from "../scripts/three/examples/jsm/controls/OrbitControls.js";
 // import Stats from "/jsm/libs/stats.module.js";
 
-let scene, camera, renderer;
+let scene, camera, renderer, hemiLight, spotLight;
 
 function main() {
   scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff);
 
   const windowsWidth = window.innerWidth;
   const windowsHeight = window.innerHeight;
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(windowsWidth, windowsHeight);
+  renderer.shadowMap.enabled = true;
+
   document.body.appendChild(renderer.domElement);
+
+  const hemiLight = new THREE.HemisphereLight(0xffff33, 0x5b573d, 2.5);
+  scene.add(hemiLight);
+
+  const spotLight1 = new THREE.SpotLight(0xffffff, 3);
+  spotLight1.castShadow = true;
+  spotLight1.shadow.bias = -0.0001;
+  spotLight1.shadow.mapSize.width = 4096;
+  spotLight1.shadow.mapSize.height = 4096;
+  scene.add(spotLight1);
+
+  renderer.toneMapping = THREE.ReinhardToneMapping;
+  renderer.toneMappingExposure = 2;
+  renderer.shadowMap.enabled = true;
 
   const aspect = windowsWidth / windowsHeight;
   camera = new THREE.PerspectiveCamera(90, aspect, 0.1, 40000);
-  camera.position.z = 1;
+  camera.position.set(-3000, 200, 1000);
 
   let control = new THREE.OrbitControls(camera, renderer.domElement);
-//   controls.target.set(0, 0, 0);
-  
-  control.minDistance = 0;
-  control.maxDistance = 200
+  //   controls.target.set(0, 0, 0);
+
+  // control.minDistance = 100;
+  // control.maxDistance = 200;
   control.update();
 
-
   let materialArrays = [];
+  {
+    let texture_ft = new THREE.TextureLoader().load(
+      `images/skybox1/greenery/meadow_ft.jpg`
+    );
+    let texture_bk = new THREE.TextureLoader().load(
+      `images/skybox1/greenery/meadow_bk.jpg`
+    );
+    let texture_up = new THREE.TextureLoader().load(
+      `images/skybox1/greenery/meadow_up.jpg`
+    );
+    let texture_dn = new THREE.TextureLoader().load(
+      `images/skybox1/greenery/meadow_dn.jpg`
+    );
+    let texture_rt = new THREE.TextureLoader().load(
+      `images/skybox1/greenery/meadow_rt.jpg`
+    );
+    let texture_lf = new THREE.TextureLoader().load(
+      `images/skybox1/greenery/meadow_lf.jpg`
+    );
 
-   let texture_ft = new THREE.TextureLoader().load( `images/skybox1/greenery/meadow_ft.jpg`);
-   let texture_bk = new THREE.TextureLoader().load( `images/skybox1/greenery/meadow_bk.jpg`);
-   let texture_up = new THREE.TextureLoader().load( `images/skybox1/greenery/meadow_up.jpg`);
-   let texture_dn = new THREE.TextureLoader().load( `images/skybox1/greenery/meadow_dn.jpg`);
-   let texture_rt = new THREE.TextureLoader().load( `images/skybox1/greenery/meadow_rt.jpg`);
-   let texture_lf = new THREE.TextureLoader().load( `images/skybox1/greenery/meadow_lf.jpg`);
+    materialArrays.push(
+      new THREE.MeshBasicMaterial({ map: texture_ft, side: THREE.BackSide })
+    );
+    materialArrays.push(
+      new THREE.MeshBasicMaterial({ map: texture_bk, side: THREE.BackSide })
+    );
+    materialArrays.push(
+      new THREE.MeshBasicMaterial({ map: texture_up, side: THREE.BackSide })
+    );
+    materialArrays.push(
+      new THREE.MeshBasicMaterial({ map: texture_dn, side: THREE.BackSide })
+    );
+    materialArrays.push(
+      new THREE.MeshBasicMaterial({ map: texture_rt, side: THREE.BackSide })
+    );
+    materialArrays.push(
+      new THREE.MeshBasicMaterial({ map: texture_lf, side: THREE.BackSide })
+    );
+  }
 
-
-	materialArrays.push(new THREE.MeshBasicMaterial({ map: texture_ft, side:THREE.BackSide }));
-	materialArrays.push(new THREE.MeshBasicMaterial({ map: texture_bk,  side:THREE.BackSide }));
-	materialArrays.push(new THREE.MeshBasicMaterial({ map: texture_up,  side:THREE.BackSide }));
-	materialArrays.push(new THREE.MeshBasicMaterial({ map: texture_dn,  side:THREE.BackSide }));
-	materialArrays.push(new THREE.MeshBasicMaterial({ map: texture_rt,  side:THREE.BackSide }));
-	materialArrays.push(new THREE.MeshBasicMaterial({ map: texture_lf,  side:THREE.BackSide }));
-
+  scene.add(new THREE.AxesHelper(5000));
   const geometry = new THREE.BoxGeometry(1000, 1000, 1000);
-  let skyBox = new THREE.Mesh(geometry,  materialArrays );
-  scene.add(skyBox);
+  let skyBox = new THREE.Mesh(geometry, materialArrays);
+  // scene.add(skyBox);
 
-  draw();
+  new THREE.GLTFLoader().load("models/iguana/scene.gltf", (result) => {
+    let model = result.scene.children[0];
+    model.position.set(100.0, 2.0, -3.0);
+    model.traverse((object1) => {
+      if (object1.isMesh) {
+        object1.castShadow = true;
+        object1.receiveShadow = true;
+
+        if (object1.material.map) {
+          object1.material.map.anisotropy = 16;
+        }
+      }
+    });
+    // spotLight1.target = model;
+    scene.add(model);
+    draw();
+  });
 
   function draw() {
-	  console.log(camera.position.z)
+    spotLight1.position.set(
+      camera.position.x + 25,
+      camera.position.y + 500,
+      camera.position.z + 25
+    );
     renderer.render(scene, camera);
     requestAnimationFrame(draw);
   }
-  }
+}
 
 main();
--- public/dist/client.dev.js
+++ public/dist/client.dev.js
@@ -1,54 +1,97 @@
 "use strict";
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-var THREE = _interopRequireWildcard(require("/build/three.module.js"));
-
-var _OrbitControls = require("/jsm/controls/OrbitControls.js");
-
-var _statsModule = _interopRequireDefault(require("/jsm/libs/stats.module.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
-var url = new URL("/build/three.module.js");
-console.log(url);
-var scene, camera, renderer;
+// import * as THREE from "/build/three.module.js";
+// import { OrbitControls } from "../scripts/three/examples/jsm/controls/OrbitControls.js";
+// import Stats from "/jsm/libs/stats.module.js";
+var scene, camera, renderer, hemiLight, spotLight;
 
 function main() {
   scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff);
   var windowsWidth = window.innerWidth;
   var windowsHeight = window.innerHeight;
   renderer = new THREE.WebGLRenderer({
     antialias: true
   });
   renderer.setSize(windowsWidth, windowsHeight);
+  renderer.shadowMap.enabled = true;
   document.body.appendChild(renderer.domElement);
+  var hemiLight = new THREE.HemisphereLight(0xffff33, 0x5b573d, 2.5);
+  scene.add(hemiLight);
+  var spotLight1 = new THREE.SpotLight(0xffffff, 3);
+  spotLight1.castShadow = true;
+  spotLight1.shadow.bias = -0.0001;
+  spotLight1.shadow.mapSize.width = 4096;
+  spotLight1.shadow.mapSize.height = 4096;
+  scene.add(spotLight1);
+  renderer.toneMapping = THREE.ReinhardToneMapping;
+  renderer.toneMappingExposure = 2;
+  renderer.shadowMap.enabled = true;
   var aspect = windowsWidth / windowsHeight;
-  camera = new THREE.PerspectiveCamera(90, aspect, 0.1, 1000);
-  camera.position.z = 2; // camera.position.set(-900, -200, -900);
+  camera = new THREE.PerspectiveCamera(90, aspect, 0.1, 40000);
+  camera.position.set(-3000, 200, 1000);
+  var control = new THREE.OrbitControls(camera, renderer.domElement); //   controls.target.set(0, 0, 0);
+  // control.minDistance = 100;
+  // control.maxDistance = 200;
 
-  var controls = new _OrbitControls.OrbitControls(camera, renderer.domElement);
-  controls.addEventListener("change", renderer);
+  control.update();
   var materialArrays = [];
-  var textureImages = ["meadow_ft.jpg", "meadow_bk.jpg", "meadow_up.jpg", "meadow_dn.jpg", "meadow_rt.jpg", "meadow_lf.jpg"];
-
-  for (var i = 0; i < 6; i++) {
-    var texture = new THREE.TextureLoader().load("../images/skyboxday1/greenery/".concat(textureImages[i]));
-    var material = new THREE.MeshBasicMaterial({
-      map: texture
-    });
-    materialArrays.push(material);
+  {
+    var texture_ft = new THREE.TextureLoader().load("images/skybox1/greenery/meadow_ft.jpg");
+    var texture_bk = new THREE.TextureLoader().load("images/skybox1/greenery/meadow_bk.jpg");
+    var texture_up = new THREE.TextureLoader().load("images/skybox1/greenery/meadow_up.jpg");
+    var texture_dn = new THREE.TextureLoader().load("images/skybox1/greenery/meadow_dn.jpg");
+    var texture_rt = new THREE.TextureLoader().load("images/skybox1/greenery/meadow_rt.jpg");
+    var texture_lf = new THREE.TextureLoader().load("images/skybox1/greenery/meadow_lf.jpg");
+    materialArrays.push(new THREE.MeshBasicMaterial({
+      map: texture_ft,
+      side: THREE.BackSide
+    }));
+    materialArrays.push(new THREE.MeshBasicMaterial({
+      map: texture_bk,
+      side: THREE.BackSide
+    }));
+    materialArrays.push(new THREE.MeshBasicMaterial({
+      map: texture_up,
+      side: THREE.BackSide
+    }));
+    materialArrays.push(new THREE.MeshBasicMaterial({
+      map: texture_dn,
+      side: THREE.BackSide
+    }));
+    materialArrays.push(new THREE.MeshBasicMaterial({
+      map: texture_rt,
+      side: THREE.BackSide
+    }));
+    materialArrays.push(new THREE.MeshBasicMaterial({
+      map: texture_lf,
+      side: THREE.BackSide
+    }));
   }
+  scene.add(new THREE.AxesHelper(5000));
+  var geometry = new THREE.BoxGeometry(1000, 1000, 1000);
+  var skyBox = new THREE.Mesh(geometry, materialArrays); // scene.add(skyBox);
 
-  var geometry = new THREE.boxGeometry(10000, 10000, 10000);
-  var skyBox = new THREE.Mesh(geometry, materialArrays);
-  scene.add(skyBox);
+  new THREE.GLTFLoader().load("models/iguana/scene.gltf", function (result) {
+    var model = result.scene.children[0];
+    model.position.set(100.0, 2.0, -3.0);
+    model.traverse(function (object1) {
+      if (object1.isMesh) {
+        object1.castShadow = true;
+        object1.receiveShadow = true;
+
+        if (object1.material.map) {
+          object1.material.map.anisotropy = 16;
+        }
+      }
+    }); // spotLight1.target = model;
+
+    scene.add(model);
+    draw();
+  });
 
   function draw() {
+    spotLight1.position.set(camera.position.x + 25, camera.position.y + 500, camera.position.z + 25);
     renderer.render(scene, camera);
     requestAnimationFrame(draw);
   }
