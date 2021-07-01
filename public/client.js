092766c6f0aa182e731f7224b4ca1f8670d2dd77
alok <alksbd9@gmail.com>
2021-07-01 19:10:45 +0545

    refactored to oop

--- public/client.js
+++ public/client.js
@@ -1,104 +1,145 @@
-// import * as THREE from "/build/three.module.js";
-// import { OrbitControls } from "../scripts/three/examples/jsm/controls/OrbitControls.js";
-// import Stats from "/jsm/libs/stats.module.js";
+class GameEngine {
+  constructor(scene, renderer, camera){
+    this.scene = scene
+    this.renderer = renderer
+    this.camera = camera
+  }
 
-let scene, camera, renderer, hemiLight, spotLight;
+  run() {
+    this.runGameLoop()
+  }
 
-function main() {
-  scene = new THREE.Scene();
+  runGameLoop() {
+    this.updateGame()
+    this.updatePhysics()
+    this.render()
+    requestAnimationFrame(() => this.runGameLoop());
+  }
 
-  let cubeTexture = new THREE.CubeTextureLoader()
-    .setPath("images/skybox1/greenery/")
-    .load([
-      "meadow_ft.jpg",
-      "meadow_bk.jpg",
-      "meadow_up.jpg",
-      "meadow_dn.jpg",
-      "meadow_rt.jpg",
-      "meadow_lf.jpg",
-    ]);
-  scene.background = cubeTexture;
+  updateGame() {}
 
-  const windowsWidth = window.innerWidth;
-  const windowsHeight = window.innerHeight;
+  updatePhysics() {}
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(windowsWidth, windowsHeight);
-  renderer.shadowMap.enabled = true;
+  render() {
+    this.renderer.render(this.scene.scene, this.camera)
+  }
+}
 
-  document.body.appendChild(renderer.domElement);
+class Scene {
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+    this.gameObjects = []
+    this.createScene()
+  }
 
+  createScene() {
+    this.scene = new THREE.Scene();
+  }
+
+  addSkyBox(skybox) {
+    this.scene.background = skybox.cubeTexture;
+  }
+
+  addGameObject(gameObject) {
+    this.scene.add(gameObject)
+    this.gameObjects.push(gameObject)
+  }
+}
+
+class Renderer {
+  constructor(width, height) {
+    this.createRenderer()
+    this.setupSize(width, height)
+    this.enableShadowMap()
+    this.addToDocument()
+  }
+
+  setupSize(width, height) {
+    this.renderer.setSize(width, height);
+  }
+
+  enableShadowMap() {
+    this.renderer.shadowMap.enabled = true;
+  }
+  
+  createRenderer() {
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+  }
+
+  addToDocument() {
+    document.body.appendChild(this.renderer.domElement);
+  }
+
+  setupToneMappingWithExposure(exposure) {
+    this.renderer.toneMapping = THREE.ReinhardToneMapping;
+    this.renderer.toneMappingExposure = exposure;
+  }
+
+  render(scene, camera) {
+    this.renderer.render(scene, camera);
+  }
+}
+
+class Skybox {
+  constructor(path, images) {
+    this.cubeTexture = new THREE.CubeTextureLoader()
+    .setPath(path)
+    .load(images);
+  }
+}
+
+const windowsWidth = window.innerWidth;
+const windowsHeight = window.innerHeight;
+
+
+function addSkyBoxToScene(scene) {
+  let path = "images/skybox1/greenery/"
+  let images = [
+    "meadow_ft.jpg",
+    "meadow_bk.jpg",
+    "meadow_up.jpg",
+    "meadow_dn.jpg",
+    "meadow_rt.jpg",
+    "meadow_lf.jpg",
+  ]
+  let skybox = new Skybox(path, images)
+  scene.addSkyBox(skybox)
+}
+
+function addHemiLight(scene) {
   const hemiLight = new THREE.HemisphereLight(0xffff33, 0x5b573d, 2.5);
-  scene.add(hemiLight);
+  scene.addGameObject(hemiLight);
+}
 
+function addSpotLight(scene) {
   const spotLight1 = new THREE.SpotLight(0xffffff, 3);
   spotLight1.castShadow = true;
   spotLight1.shadow.bias = -0.0001;
   spotLight1.shadow.mapSize.width = 4096;
   spotLight1.shadow.mapSize.height = 4096;
-  scene.add(spotLight1);
+  scene.addGameObject(spotLight1);
+}
 
-  renderer.toneMapping = THREE.ReinhardToneMapping;
-  renderer.toneMappingExposure = 2;
-  renderer.shadowMap.enabled = true;
+function addLightToScene(scene) {
+  addHemiLight(scene)
+  addSpotLight(scene)
+}
 
+function createCamera() {
   const aspect = windowsWidth / windowsHeight;
-  camera = new THREE.PerspectiveCamera(90, aspect, 0.1, 40000);
+  let camera = new THREE.PerspectiveCamera(90, aspect, 0.1, 40000);
   camera.position.set(-3000, 200, 1000);
 
-  let control = new THREE.OrbitControls(camera, renderer.domElement);
-  //   controls.target.set(0, 0, 0);
+  return camera
+}
 
-  // control.minDistance = 100;
-  // control.maxDistance = 200;
+function setupCameraControl(camera, renderer) {
+  let control = new THREE.OrbitControls(camera, renderer.renderer.domElement);
   control.update();
+}
 
-  let materialArrays = [];
-  {
-    let texture_ft = new THREE.TextureLoader().load(
-      `images/skybox1/greenery/meadow_ft.jpg`
-    );
-    let texture_bk = new THREE.TextureLoader().load(
-      `images/skybox1/greenery/meadow_bk.jpg`
-    );
-    let texture_up = new THREE.TextureLoader().load(
-      `images/skybox1/greenery/meadow_up.jpg`
-    );
-    let texture_dn = new THREE.TextureLoader().load(
-      `images/skybox1/greenery/meadow_dn.jpg`
-    );
-    let texture_rt = new THREE.TextureLoader().load(
-      `images/skybox1/greenery/meadow_rt.jpg`
-    );
-    let texture_lf = new THREE.TextureLoader().load(
-      `images/skybox1/greenery/meadow_lf.jpg`
-    );
-
-    materialArrays.push(
-      new THREE.MeshBasicMaterial({ map: texture_ft, side: THREE.BackSide })
-    );
-    materialArrays.push(
-      new THREE.MeshBasicMaterial({ map: texture_bk, side: THREE.BackSide })
-    );
-    materialArrays.push(
-      new THREE.MeshBasicMaterial({ map: texture_up, side: THREE.BackSide })
-    );
-    materialArrays.push(
-      new THREE.MeshBasicMaterial({ map: texture_dn, side: THREE.BackSide })
-    );
-    materialArrays.push(
-      new THREE.MeshBasicMaterial({ map: texture_rt, side: THREE.BackSide })
-    );
-    materialArrays.push(
-      new THREE.MeshBasicMaterial({ map: texture_lf, side: THREE.BackSide })
-    );
-  }
-
-  scene.add(new THREE.AxesHelper(5000));
-  const geometry = new THREE.BoxGeometry(1000, 1000, 1000);
-  let skyBox = new THREE.Mesh(geometry, materialArrays);
-  // scene.add(skyBox);
-
+function addModelToScene(scene) {
   new THREE.GLTFLoader().load("models/iguana/scene.gltf", (result) => {
     let model = result.scene.children[0];
     model.position.set(100.0, 2.0, -3.0);
@@ -112,20 +153,33 @@ function main() {
         }
       }
     });
-    // spotLight1.target = model;
-    scene.add(model);
-    draw();
-  });
+    scene.addGameObject(model);
+  })
+}
 
-  function draw() {
-    spotLight1.position.set(
-      camera.position.x + 25,
-      camera.position.y + 500,
-      camera.position.z + 25
-    );
-    renderer.render(scene, camera);
-    requestAnimationFrame(draw);
-  }
+function createRenderer() {
+  let renderer = new Renderer(windowsWidth, windowsHeight)
+  renderer.setupToneMappingWithExposure(2)
+  return renderer
+}
+
+function createScene() {
+  let scene = new Scene(windowsWidth, windowsHeight)
+  addLightToScene(scene)
+  addSkyBoxToScene(scene)
+  addModelToScene(scene)
+  return scene
+}
+
+function main() {
+  let renderer = createRenderer()
+  let scene = createScene()
+  let camera = createCamera()
+
+  setupCameraControl(camera, renderer)
+
+  let engine = new GameEngine(scene, renderer, camera)
+  engine.run()
 }
 
 main();
