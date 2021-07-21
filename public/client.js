bf974b34e3a8d3ca3a1ee9708cafb53003e3ae90
alok <alksbd9@gmail.com>
2021-07-21 15:44:48 +0545

    gitignore

--- public/client.js
+++ public/client.js
@@ -93,30 +93,6 @@ function createScene() {
   return scene;
 }
 
-
-class CarCreator {
-  constructor(url, loader){
-    this.url = url
-    this.loader = loader
-    this.gltf = null
-  }
-
-  loadModelFromUrl(onLoad) {
-    let self = this
-    this.loader.load(this.url, (gltf) => {
-      self.gltf = gltf
-      onLoad()
-    })
-  }
-
-  getCar() {
-    if(this.gltf != null) {
-      let car = new Car(this.gltf)
-      return car
-    }
-  }
-}
-
 function main() {
   let renderer = createRenderer();
   let scene = createScene();
